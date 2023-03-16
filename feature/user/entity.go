3890004ae0d93f35a914a597ce1fd124dc390f61
kgriffinh <griffinh29@gmail.com>
2023-03-16 14:54:19 +0800

    chore: entity user done

--- feature/user/entity.go
+++ feature/user/entity.go
@@ -1,1 +1,42 @@
 package user
+
+import (
+	"mime/multipart"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Core struct {
+	ID             uint
+	Name           string
+	Email          string `validate:"required,email"`
+	Password       string `validate:"min=5"`
+	Role           string `validate:"min=4"`
+	ProfilePicture string
+	Phone          string
+	Address        string
+}
+
+type UserHandler interface {
+	Login() echo.HandlerFunc
+	Register() echo.HandlerFunc
+	Profile() echo.HandlerFunc
+	Delete() echo.HandlerFunc
+	Update() echo.HandlerFunc
+}
+
+type UserService interface {
+	Login(email, password string) (string, Core, error)
+	Register(newUser Core) (Core, error)
+	Profile(token interface{}) (Core, error)
+	Update(token interface{}, fileData *multipart.FileHeader, updateData Core) (Core, error)
+	Delete(token interface{}) error
+}
+
+type UserData interface {
+	Login(email string) (Core, error)
+	Register(newUser Core) (Core, error)
+	Profile(userID uint) (Core, error)
+	Update(userID uint, updateData Core) (Core, error)
+	Delete(userID uint) error
+}
