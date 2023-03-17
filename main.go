f4499cb2433c9426ac15a6b6b665270b40415245
kgriffinh <griffinh29@gmail.com>
2023-03-17 14:34:40 +0800

    chore: set up route for user & homestay

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"airbnb/config"
+	homeData "airbnb/feature/homestay/data"
+	homeHdl "airbnb/feature/homestay/handler"
+	homeSrv "airbnb/feature/homestay/service"
+	usrData "airbnb/feature/user/data"
+	usrHdl "airbnb/feature/user/handler"
+	usrSrv "airbnb/feature/user/service"
+	"airbnb/helper"
 	"log"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -14,11 +22,20 @@ func main() {
 	db := config.InitDB(*cfg)
 	config.Migrate(db)
 
-	// v := validator.New()
-	// cld := helper.NewCloudinary(cfg)
+	v := validator.New()
+	cld := helper.NewCloudinary(cfg)
 	// coreapiMidtrans := helper.NewCoreMidtrans(cfg)
 
 	config.Migrate(db)
+
+	// SETUP
+	uData := usrData.New(db)
+	uSrv := usrSrv.New(uData, v, cld)
+	uHdl := usrHdl.New(uSrv)
+
+	hData := homeData.New(db)
+	hSrv := homeSrv.New(hData, v, cld)
+	hHdl := homeHdl.New(hSrv)
 
 	// MIDDLEWARE
 	e.Pre(middleware.RemoveTrailingSlash())
@@ -29,6 +46,18 @@ func main() {
 	}))
 
 	// ROUTE
+	e.POST("/register", uHdl.Register())
+	e.POST("/login", uHdl.Login())
+	e.POST("/users", uHdl.Profile(), middleware.JWT([]byte(config.JWT_KEY)))
+	e.POST("/users", uHdl.Update(), middleware.JWT([]byte(config.JWT_KEY)))
+	e.POST("/users", uHdl.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
+
+	e.POST("/homestays", hHdl.Add(), middleware.JWT([]byte(config.JWT_KEY)))
+	e.POST("/homestays", hHdl.List())
+	e.POST("/homestays", hHdl.GetbyID())
+	e.POST("/homestays", hHdl.Update(), middleware.JWT([]byte(config.JWT_KEY)))
+	e.POST("/homestays", hHdl.Delete(), middleware.JWT([]byte(config.JWT_KEY)))
+	e.POST("/homestays", hHdl.Myhome(), middleware.JWT([]byte(config.JWT_KEY)))
 
 	if err := e.Start(":8000"); err != nil {
 		log.Println(err.Error())
