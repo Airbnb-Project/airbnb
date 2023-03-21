79d2fd3e8b3f07a35203b3a1d9c4f335c152d833
kgriffinh <griffinh29@gmail.com>
2023-03-21 17:02:06 +0800

    chore: model & query feedback complete

--- feature/feedback/data/model.go
+++ feature/feedback/data/model.go
@@ -1,1 +1,32 @@
 package data
+
+import (
+	"airbnb/feature/feedback"
+
+	"gorm.io/gorm"
+)
+
+type Feedback struct {
+	gorm.Model
+	Rating     uint
+	UserID     uint
+	HomestayID uint
+}
+
+func DataToCore(data Feedback) feedback.Core {
+	return feedback.Core{
+		ID:         data.ID,
+		Rating:     data.Rating,
+		UserID:     data.UserID,
+		HomestayID: data.HomestayID,
+	}
+}
+
+func CoreToData(data feedback.Core) Feedback {
+	return Feedback{
+		Model:      gorm.Model{ID: data.ID},
+		Rating:     data.Rating,
+		UserID:     data.UserID,
+		HomestayID: data.HomestayID,
+	}
+}
--- feature/feedback/data/query.go
+++ feature/feedback/data/query.go
@@ -1,1 +1,47 @@
 package data
+
+import (
+	"airbnb/feature/feedback"
+	"errors"
+	"log"
+
+	"gorm.io/gorm"
+)
+
+type feedbackQuery struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) feedback.FeedbackData {
+	return &feedbackQuery{db: db}
+}
+
+func (fq *feedbackQuery) Add(userID uint, newFeedback feedback.Core) error {
+	cnv := CoreToData(newFeedback)
+	cnv.UserID = userID
+	err := fq.db.Create(&cnv).Error
+	if err != nil {
+		log.Println("add feedback query error", err.Error())
+		return errors.New("cannot add feedback")
+	}
+
+	newFeedback.ID = cnv.ID
+
+	return nil
+}
+
+func (fq *feedbackQuery) List(userID uint, homestayID uint) ([]feedback.Core, error) {
+	fb := []Feedback{}
+	err := fq.db.Where("homestay_id = ?", homestayID).Order("created_at DESC").Find(&fb).Error
+	if err != nil {
+		log.Println("show feedback query error", err.Error())
+		return []feedback.Core{}, errors.New("cannot show list feedback")
+	}
+
+	list := []feedback.Core{}
+	for _, v := range fb {
+		list = append(list, DataToCore(v))
+	}
+
+	return list, nil
+}
