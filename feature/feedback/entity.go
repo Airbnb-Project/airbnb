a6859fff3ad9f2978c13b4b153f5b530ddee64eb
kgriffinh <griffinh29@gmail.com>
2023-03-21 16:44:33 +0800

    chore: create entity feedback

--- feature/feedback/entity.go
+++ feature/feedback/entity.go
@@ -1,1 +1,25 @@
 package feedback
+
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID         uint
+	Rating     uint
+	UserID     uint
+	HomestayID uint
+}
+
+type FeedbackHandler interface {
+	Add() echo.HandlerFunc
+	List() echo.HandlerFunc
+}
+
+type FeedbackService interface {
+	Add(token interface{}, homestayID uint, newFeedback Core) error
+	List(token interface{}, homestayID uint) ([]Core, error)
+}
+
+type FeedbackData interface {
+	Add(userID uint, newFeedback Core) error
+	List(userID uint, homestayID uint) ([]Core, error)
+}
