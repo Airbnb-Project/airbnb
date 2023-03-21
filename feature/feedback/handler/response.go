2f8415b52f1131bcfb4a8cd8dbfb08937ed5f983
kgriffinh <griffinh29@gmail.com>
2023-03-21 17:20:20 +0800

    chore: create feedback response

--- feature/feedback/handler/response.go
+++ feature/feedback/handler/response.go
@@ -1,1 +1,9 @@
 package handler
+
+type FeedbackResponse struct {
+	ID         uint   `json:"id"`
+	Rating     uint   `json:"rating"`
+	UserID     uint   `json:"user_id"`
+	HomestayID uint   `json:"homestay_id"`
+	Name       string `json:"name"`
+}
