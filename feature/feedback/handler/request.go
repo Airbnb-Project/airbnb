e5d49467dd1002fe970838468ca1bdf0a1ca39cf
kgriffinh <griffinh29@gmail.com>
2023-03-21 17:17:19 +0800

    chore: create feedback request

--- feature/feedback/handler/request.go
+++ feature/feedback/handler/request.go
@@ -1,1 +1,7 @@
 package handler
+
+type AddRequest struct {
+	UserID     uint `json:"user_id" form:"user_id"`
+	HomestayID uint `json:"homestay_id" form:"homestay_id"`
+	Rating     uint `json:"rating" form:"rating"`
+}
