5019e1b77a277152559f1e96c5e0adaa360ccaec
kgriffinh <griffinh29@gmail.com>
2023-03-25 14:30:26 +0800

    chore: setup handler

--- feature/reservation/handler/handler.go
+++ feature/reservation/handler/handler.go
@@ -1,1 +1,51 @@
 package handler
+
+import (
+	"airbnb/feature/reservation"
+
+	"github.com/labstack/echo/v4"
+)
+
+type reservationHandler struct {
+	srv reservation.ReservationService
+}
+
+func New(r reservation.ReservationService) reservation.ReservationHandler {
+	return &reservationHandler{srv: r}
+}
+
+func (rh *reservationHandler) Create() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
+
+func (rh *reservationHandler) List() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
+
+func (rh *reservationHandler) Detail() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
+
+func (rh *reservationHandler) Accept() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
+
+func (rh *reservationHandler) Cancel() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
+
+func (rh *reservationHandler) Callback() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
