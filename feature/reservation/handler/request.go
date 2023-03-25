5fce835447d33213197b04e0ac268d4c4690e2c8
kgriffinh <griffinh29@gmail.com>
2023-03-25 14:05:47 +0800

    chore: request complete

--- feature/reservation/handler/request.go
+++ feature/reservation/handler/request.go
@@ -1,1 +1,14 @@
 package handler
+
+type rsvRequest struct {
+	HomestayID uint   `json:"homestay_id" form:"homestay_id"`
+	Checkin    string `json:"checkin" form:"checkin"`
+	Checkout   string `json:"checkout" form:"checkout"`
+	TotalPrice int    `json:"total_price" form:"total_price"`
+	Bank       string `json:"bank" form:"bank"`
+}
+
+type Callback struct {
+	OrderID           uint   `json:"order_id" form:"order_id"`
+	TransactionStatus string `json:"transaction_status" form:"transaction_status"`
+}
