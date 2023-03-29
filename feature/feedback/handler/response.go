4ae7ed84b8bb5547da0f226afb20198dba2b216e
kgriffinh <griffinh29@gmail.com>
2023-03-29 16:52:25 +0800

    chore: change response feedback

--- feature/feedback/handler/response.go
+++ feature/feedback/handler/response.go
@@ -1,9 +1,12 @@
 package handler
 
 type FeedbackResponse struct {
-	ID         uint   `json:"id"`
-	Rating     uint   `json:"rating"`
-	Note       string `json:"note"`
-	UserID     uint   `json:"user_id"`
-	HomestayID uint   `json:"homestay_id"`
+	ID              uint   `json:"id"`
+	Rating          uint   `json:"rating"`
+	Note            string `json:"note"`
+	UserID          uint   `json:"user_id"`
+	UserName        string `json:"user_name"`
+	HomestayID      uint   `json:"homestay_id"`
+	HomestayName    string `json:"homestay_name"`
+	HomestayAddress string `json:"homestay_address"`
 }
