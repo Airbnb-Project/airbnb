0e99769b38c359afbfda94c1b89e133e06e988c2
kgriffinh <griffinh29@gmail.com>
2023-03-31 18:25:04 +0800

    chore: add model reservation

--- feature/reservation/data/model.go
+++ feature/reservation/data/model.go
@@ -20,6 +20,54 @@ type Reservation struct {
 	VAnumber        string
 }
 
+type ReservationModel struct {
+	ID              uint
+	UserID          uint
+	HomestayID      uint
+	Name            string
+	Image           string
+	Address         string
+	Ticket          string
+	Checkin         string
+	Checkout        string
+	Guest           int
+	ReservationDate string
+	TotalPrice      int
+	Status          string
+	Bank            string
+	VAnumber        string
+}
+
+func ToCore(data ReservationModel) reservation.Core {
+	return reservation.Core{
+		ID:              data.ID,
+		UserID:          data.UserID,
+		HomestayID:      data.HomestayID,
+		Name:            data.Name,
+		Image:           data.Image,
+		Address:         data.Address,
+		Ticket:          data.Ticket,
+		Checkin:         data.Checkin,
+		Checkout:        data.Checkout,
+		Guest:           data.Guest,
+		ReservationDate: data.ReservationDate,
+		TotalPrice:      data.TotalPrice,
+		Status:          data.Status,
+		Bank:            data.Bank,
+		VAnumber:        data.VAnumber,
+	}
+}
+
+func ListToCore(data []ReservationModel) []reservation.Core {
+	core := []reservation.Core{}
+
+	for _, v := range data {
+		core = append(core, ToCore(v))
+	}
+
+	return core
+}
+
 func DataToCore(data Reservation) reservation.Core {
 	return reservation.Core{
 		ID:              data.ID,
